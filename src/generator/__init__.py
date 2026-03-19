3f07e1bfed13b0764efbba86f3d6b3d1e417eaa2
swth1991 <swth1991@gmail.com>
2026-03-19 10:45:55 +0900

    사용하지 않는 TypeHandlerGenerator 관련 코드 삭제

--- src/generator/__init__.py
+++ src/generator/__init__.py
@@ -2,15 +2,9 @@
 Generator 모듈
 
 암복호화 관련 코드를 자동 생성하는 모듈입니다.
-- TypeHandlerGenerator: MyBatis Type Handler 생성 (해당 모듈 존재 시)
 - CheckJoinRunner: access_tables 기준 JOIN 대상 테이블/컬럼 분석 (check_join 명령)
 """
 
 from .check_join import CheckJoinRunner
 
-try:
-    from .type_handler_generator import TypeHandlerGenerator
-    __all__ = ["TypeHandlerGenerator", "CheckJoinRunner"]
-except ImportError:
-    TypeHandlerGenerator = None  # type: ignore[misc, assignment]
-    __all__ = ["CheckJoinRunner"]
+__all__ = ["CheckJoinRunner"]
